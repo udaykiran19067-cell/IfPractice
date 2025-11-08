e5f2f182b9d03c09e0aa70698928c4113a7535da
udayk <udayk@UDAYKIRAN>
2025-11-08 15:57:34 +0530

    Add project files.

---
+++ IfPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IfPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] emp = new string[] { "vijay", "vkinay", "vikas", "Vidya" };
+            Console.WriteLine("Employee names:");
+
+           foreach(var name in emp) 
+            {
+                Console.WriteLine(name);
+                if (name == "vikas")
+
+
+                {
+            
+                    break;
+                }
+            }
+        }
+    }
+}
