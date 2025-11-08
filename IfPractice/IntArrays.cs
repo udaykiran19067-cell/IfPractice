bdd46aafb51b2ee8a650683dda1857c05e856184
UDAY KIRAN <udaykiran038@outlook.com>
2025-11-08 16:34:10 +0530

    Added and practiced Arrays.

---
+++ IfPractice/IntArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfPractice
+{
+    public class IntArrays
+    {
+        public void intArrayMethod()
+        {
+            // Creating an integer array
+            int[] numbers = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            Console.WriteLine("Integer Array elements:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+                if (num == 70)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
